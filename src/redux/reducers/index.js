bc0d682a2c3b2b37e19dc54759d9b63680bee320
MarcoAlayn <marc_rob92@hotmail.com>
2022-11-10 17:35:59 -0700

    first commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,14 @@
+// import the actions defined
+
+const initialState = {};
+
+const rootReducer = (state = initialState, action) => {
+  switch (key) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
+export default rootReducer;
