6da429b88c0a7b1d1d4dee16c4c68a1f1c3e5c23
MarcoAlayn <marc_rob92@hotmail.com>
2022-11-14 01:27:26 -0700

    footer - grid system

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./footer.css";
 import { useSelector } from "react-redux";
+import { ReactComponent as FacebookIcon } from "../../assets/iconmonstr-facebook-1.svg";
+import { ReactComponent as InstagramIcon } from "../../assets/iconmonstr-instagram-11.svg";
+import { ReactComponent as TwitterIcon } from "../../assets/iconmonstr-twitter-1.svg";
 
 function Footer() {
   const lang = useSelector((state) => state.lang);
@@ -25,30 +28,49 @@ function Footer() {
 
   return (
     <div className="footerContainer">
-      <p>{contactCenter?.title}</p>
-      <span>{contactCenter?.email} </span>
-
-      <div className="numbersNsocial">
-        <div className="contacts">
-          {numbers?.map((e) => {
-            return (
-              <p key={e.name}>
-                {e.name}
-                <span>{e.number}</span>
-              </p>
-            );
-          })}
-        </div>
-
-        <div className="social">
-          <a href={social?.facebookUrl}>facebook</a>
-          <a href={social?.instagramUrl}>instagram</a>
-          <a href={social?.twitterUrl}>twitter</a>
-        </div>
+      <div className="contactMail">
+        <p>{contactCenter?.title}</p>
+        <span>{contactCenter?.email} </span>
       </div>
 
+      <div className="numbers">
+        {numbers ? (
+          <>
+            <p>{numbers[0]?.name}</p>
+            <p>{numbers[0]?.number}</p>
+            <p>{numbers[1]?.name}</p>
+            <p>{numbers[1]?.number}</p>
+            <p>{numbers[2]?.name}</p>
+            <p>{numbers[2]?.number}</p>
+            <p>{numbers[3]?.name}</p>
+            <p>{numbers[3]?.number}</p>
+            <p>{numbers[4]?.name}</p>
+            <p>{numbers[4]?.number}</p>
+            <p>{numbers[5]?.name}</p>
+            <p>{numbers[5]?.number}</p>
+            <p>{numbers[6]?.name}</p>
+            <p>{numbers[6]?.number}</p>
+            <p>{numbers[7]?.name}</p>
+            <p>{numbers[7]?.number}</p>
+          </>
+        ) : (
+          <h3>Loading</h3>
+        )}
+      </div>
+
+      <div className="social">
+        <a href={social?.facebookUrl}>
+          <FacebookIcon />
+        </a>
+        <a href={social?.instagramUrl}>
+          <InstagramIcon />
+        </a>
+        <a href={social?.twitterUrl}>
+          <TwitterIcon />
+        </a>
+      </div>
       <div className="termsUse">
-        <p>{footer?.copy}</p>
+        <p className="firstChild">{footer?.copy}</p>
         <p>
           <a href={footer?.links[0].href}>{footer?.links[0].tittle}</a>
         </p>
