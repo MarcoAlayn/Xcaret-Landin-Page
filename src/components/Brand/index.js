ce64f9565a5b91194bc61012386e42da2d59a79e
MarcoAlayn <marc_rob92@hotmail.com>
2022-11-16 22:31:09 -0700

    navbar mobile responsive

--- src/components/Brand/index.js
+++ src/components/Brand/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./brand.css";
 
 // eslint-disable-next-line react/prop-types
 function Brand({ logo }) {
   return (
-    <div>
+    <div className="brandContainer">
       <img src={logo} alt="logo xcaret" />
     </div>
   );
